5d8f26a32cd4f68a1dc2bfd44e39cb6f2f257f70
Kapil Garg <gargkapil90@gmail.com>
2024-07-11 22:48:17 +0530

    adding

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -11,9 +11,29 @@
 // console.log(kapilAge);
 // console.log(kapilAge * 2, kapilAge / 10, 2 ** 3);
 
-const firstName = 'Kapil';
-const job = 'Software Engineer';
-const age = 2024 - 1990;
+// const firstName = 'Kapil';
+// const job = 'Software Engineer';
+// const age = 2024 - 1990;
 
-const kapilJob = `I'm ${firstName}, a ${age} year old ${job}.`;
-console.log(kapilJob);
+// const kapilJob = `I'm ${firstName}, a ${age} year old ${job}.`;
+// console.log(kapilJob);
+
+// console.log(20 + 10 + '30');
+// console.log('20' + 10 + 30);
+// console.log(20 + '10' + 30);
+
+//Switch
+const day = 'monda';
+switch (day) {
+    case 'monday':
+        console.log('Hey its monday');
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    default:
+        console.log('Not a valid day');
+}
+
