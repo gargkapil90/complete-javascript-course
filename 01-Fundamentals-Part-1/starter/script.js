a8c16e387f64a2576f6a360265b3640eb6a69e85
Kapil Garg <gargkapil90@gmail.com>
2024-07-11 22:15:47 +0530

    adding operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,19 @@
-let js = 'amazing';
-let firstName = 'Kapil';
+// let js = 'amazing';
+// let firstName = 'Kapil';
 
-console.log(firstName);
-console.log(null);
-console.log(typeof null);
+// console.log(firstName);
+// const DOB = 1990;
+// console.log(DOB);
+// // DOB = 1991;
+
+// const Now = 2024;
+// const kapilAge = Now - 1990;
+// console.log(kapilAge);
+// console.log(kapilAge * 2, kapilAge / 10, 2 ** 3);
+
+const firstName = 'Kapil';
+const job = 'Software Engineer';
+const age = 2024 - 1990;
+
+const kapilJob = `I'm ${firstName}, a ${age} year old ${job}.`;
+console.log(kapilJob);
